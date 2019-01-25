923bb3f62e53e198bb10ff4899330bdf8dc343ab
Ryan Marcotte <RyanM@iqmetrix.com>
2019-01-24 18:51:14 -0600

    tests and fixes

---
+++ Functional.Primitives.Extensions.Tests/_Assertions/FunctionalPrimitiveAssertions.cs
@@ -0,0 +1,33 @@
+﻿using Functional;
+
+namespace FluentAssertions
+{
+	/// <summary>
+	/// Defines additional fluent assertion gateways for types defined in Functional.Primitives namespace.
+	/// </summary>
+	public static class FunctionalPrimitiveAssertions
+	{
+		/// <summary>
+		/// Returns a <see cref="Result{TSuccess,TFailure}"/> object that can be used to assert the current <see cref="Result{TSuccess, TFailure}"/>.
+		/// </summary>
+		/// <typeparam name="TSuccess">The success object type.</typeparam>
+		/// <typeparam name="TFailure">The failure object type.</typeparam>
+		/// <param name="result">The result.</param>
+		/// <returns></returns>
+		public static ResultTypeAssertions<TSuccess, TFailure> Should<TSuccess, TFailure>(this Result<TSuccess, TFailure> result)
+		{
+			return new ResultTypeAssertions<TSuccess, TFailure>(result);
+		}
+
+		/// <summary>
+		/// Returns a <see cref="OptionTypeAssertions{T}"/> object that is used to assert the current <see cref="Option{T}"/>.
+		/// </summary>
+		/// <typeparam name="T">The option type.</typeparam>
+		/// <param name="option">The option.</param>
+		/// <returns></returns>
+		public static OptionTypeAssertions<T> Should<T>(this Option<T> option)
+		{
+			return new OptionTypeAssertions<T>(option);
+		}
+	}
+}
---
+++ Functional.Primitives.Extensions.Tests/_Assertions/ResultTypeAssertions.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using FluentAssertions;
+using FluentAssertions.Equivalency;
+using FluentAssertions.Execution;
+using Functional;
+
+namespace FluentAssertions
+{
+	/// <summary>
+	/// Defines assertions for <see cref="Result{TSuccess,TFailure}"/> type.
+	/// </summary>
+	/// <typeparam name="TSuccess">The success value type.</typeparam>
+	/// <typeparam name="TFailure">The failure value type.</typeparam>
+	public class ResultTypeAssertions<TSuccess, TFailure>
+	{
+		private readonly Result<TSuccess, TFailure> _subject;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ResultTypeAssertions{TSuccess, TFailure}"/> class.
+		/// </summary>
+		/// <param name="subject">The <see cref="Result{TSuccess, TFailure}"/> instance to verify.</param>
+		public ResultTypeAssertions(Result<TSuccess, TFailure> subject)
+		{
+			_subject = subject;
+		}
+
+		/// <summary>
+		/// Verifies that the subject <see cref="Result{TSuccess, TFailure}"/> holds a successful result.
+		/// </summary>
+		/// <param name="because">Additional information for if the assertion fails.</param>
+		/// <param name="becauseArgs">Zero or more objects to format using the placeholders in <paramref name="because"/>.</param>
+		public void BeSuccessful(string because = "", params object[] becauseArgs)
+		{
+			Execute.Assertion
+				.ForCondition(_subject.IsSuccess())
+				.BecauseOf(because, becauseArgs)
+				.FailWith($"Expected result to be successful, but received faulted result instead {{reason}}");
+		}
+
+		/// <summary>
+		/// Verifies that the subject <see cref="Result{TSuccess, TFailure}"/> holds a successful result.  If so, execute an action that can be used to perform additional assertions.
+		/// </summary>
+		/// <param name="additionalAssertionAction">The action used to perform additional assertions.</param>
+		/// <param name="because">Additional information for if the assertion fails.</param>
+		/// <param name="becauseArgs">Zero or more objects to format using the placeholders in <paramref name="because"/>.</param>
+		public void BeSuccessful(Action<TSuccess> additionalAssertionAction, string because = "", params object[] becauseArgs)
+		{
+			if (additionalAssertionAction == null) throw new ArgumentNullException(nameof(additionalAssertionAction));
+
+			BeSuccessful(because, becauseArgs);
+			additionalAssertionAction(_subject.Success().ValueOrDefault());
+		}
+
+		/// <summary>
+		/// Verifies that the subject <see cref="Result{TSuccess, TFailure}"/> holds a specific successful result.  Useful error messages require <typeparamref name="TSuccess"/> to implement ToString().
+		/// </summary>
+		/// <param name="expectedValue">The expected successful value.</param>
+		/// <param name="because">Additional information for if the assertion fails.</param>
+		/// <param name="becauseArgs">Zero or more objects to format using the placeholders in <paramref name="because"/>.</param>
+		public void BeSuccessfulWithExpectedValue(TSuccess expectedValue, string because = "", params object[] becauseArgs)
+			=> BeSuccessfulWithExpectedValue(expectedValue, options => options, because, becauseArgs);
+
+		/// <summary>
+		/// Verifies that the subject <see cref="Result{TSuccess, TFailure}"/> holds a specific successful result.  Useful error messages require <typeparamref name="TSuccess"/> to implement ToString().
+		/// </summary>
+		/// <param name="expectedValue">The expected successful value.</param>
+		/// <param name="config">A function to configure how objects are determined to be equivalent, to be used for this assertion only.</param>
+		/// <param name="because">Additional information for if the assertion fails.</param>
+		/// <param name="becauseArgs">Zero or more objects to format using the placeholders in <paramref name="because"/>.</param>
+		public void BeSuccessfulWithExpectedValue(TSuccess expectedValue, Func<EquivalencyAssertionOptions<TSuccess>, EquivalencyAssertionOptions<TSuccess>> config, string because = "", params object[] becauseArgs)
+		{
+			BeSuccessful(because, becauseArgs);
+
+			var value = _subject.Success().ValueOrDefault();
+			value.Should().BeEquivalentTo(
+				expectedValue,
+				config,
+				$"Expected result to be successful with value '{expectedValue}', but received successful result with value '{value}' instead.",
+				becauseArgs);
+		}
+
+		/// <summary>
+		/// Verifies that the subject <see cref="Result{TSuccess, TFailure}"/> holds a faulted result.
+		/// </summary>
+		/// <param name="because">Additional information for if the assertion fails.</param>
+		/// <param name="becauseArgs">Zero or more objects to format using the placeholders in <paramref name="because"/>.</param>
+		public void BeFaulted(string because = "", params object[] becauseArgs)
+		{
+			Execute.Assertion
+				.ForCondition(!_subject.IsSuccess())
+				.BecauseOf(because, becauseArgs)
+				.FailWith($"Expected result to be faulted, but received successful result instead {{reason}}");
+		}
+
+		/// <summary>
+		/// Verifies that the subject <see cref="Result{TSuccess, TFailure}"/> holds a faulted result.  If so, execute an action that can be used to perform additional assertions.
+		/// </summary>
+		/// <param name="additionalAssertionAction">The action used to perform additional assertions.</param>
+		/// <param name="because">Additional information for if the assertion fails.</param>
+		/// <param name="becauseArgs">Zero or more objects to format using the placeholders in <paramref name="because"/>.</param>
+		public void BeFaulted(Action<TFailure> additionalAssertionAction, string because = "", params object[] becauseArgs)
+		{
+			if (additionalAssertionAction == null) throw new ArgumentNullException(nameof(additionalAssertionAction));
+
+			BeFaulted(because, becauseArgs);
+			additionalAssertionAction(_subject.Failure().ValueOrDefault());
+		}
+
+		/// <summary>
+		/// Verifies that the subject <see cref="Result{TSuccess, TFailure}"/> holds a specific faulted result.  Useful error messages require <typeparamref name="TFailure"/> to implement ToString().
+		/// </summary>
+		/// <param name="expectedValue">The expected faulted value.</param>
+		/// <param name="because">Additional information for if the assertion fails.</param>
+		/// <param name="becauseArgs">Zero or more objects to format using the placeholders in <paramref name="because"/>.</param>
+		public void BeFaultedWithExpectedValue(TFailure expectedValue, string because = "", params object[] becauseArgs)
+			=> BeFaultedWithExpectedValue(expectedValue, options => options, because, becauseArgs);
+
+		/// <summary>
+		/// Verifies that the subject <see cref="Result{TSuccess, TFailure}"/> holds a specific faulted result.  Useful error messages require <typeparamref name="TFailure"/> to implement ToString().
+		/// </summary>
+		/// <param name="expectedValue">The expected faulted value.</param>
+		/// <param name="config">A function to configure how objects are determined to be equivalent, to be used for this assertion only.</param>
+		/// <param name="because">Additional information for if the assertion fails.</param>
+		/// <param name="becauseArgs">Zero or more objects to format using the placeholders in <paramref name="because"/>.</param>
+		public void BeFaultedWithExpectedValue(TFailure expectedValue, Func<EquivalencyAssertionOptions<TFailure>, EquivalencyAssertionOptions<TFailure>> config, string because = "", params object[] becauseArgs)
+		{
+			BeFaulted(because, becauseArgs);
+
+			var value = _subject.Failure().ValueOrDefault();
+			value.Should().BeEquivalentTo(
+				expectedValue,
+				config,
+				$"Expected result to be faulted with value '{expectedValue}', but received faulted result with value '{value}' instead.",
+				becauseArgs);
+		}
+	}
+}
