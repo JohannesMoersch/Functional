b05de129d0818d57cae23971fdc3215b860729c0
Johannes Moersch <Johannes.Moersch@gmail.com>
2019-03-19 19:58:39 -0600

    Lays out group function.

---
+++ Functional.Primitives.AsyncEnumerables/ResultLinqSyntaxGroupExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Functional.Primitives.AsyncEnumerables
+{
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	public static class ResultLinqSyntaxGroupExtensions
+	{
+		public static IResultEnumerable<IGrouping<TKey, TSuccess>, TFailure> GroupBy<TKey, TSuccess, TFailure>(this IResultEnumerable<TSuccess, TFailure> source, Func<TSuccess, TKey> keySelector)
+		{
+		}
+	}
+}
---
+++ Functional.Tests/Results/ResultLinqSyntaxTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Functional.Tests.Results
+{
+	public class ResultLinqSyntaxTests
+	{
+		public void SynchronousGroupBy()
+			=>
+			(
+				from num in new int[] { 1, 2, 3, 4, 5 }
+				from value in Result.Success<int, string>(num)
+				group value by value % 2 == 0 into numGroup
+				from x in numGroup
+				select x
+			)
+			.TakeUntilFailure();
+	}
+}
