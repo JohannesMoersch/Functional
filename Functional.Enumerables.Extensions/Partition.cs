49d4c650b50df758cba310b6b2da912c5c91c6ca
Johannes Moersch <Johannes.Moersch@gmail.com>
2019-04-08 22:49:48 -0600

    Adds basic partition support.

---
+++ Functional.Enumerables.Extensions/Partition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functional
+{
+	public struct Partition<T>
+	{
+		public IEnumerable<T> Matches { get; }
+
+		public IEnumerable<T> NonMatches { get; }
+
+		internal Partition(IEnumerable<T> matches, IEnumerable<T> nonMatches)
+		{
+			Matches = matches ?? throw new ArgumentNullException(nameof(matches));
+			NonMatches = nonMatches ?? throw new ArgumentNullException(nameof(nonMatches));
+		}
+
+		public void Deconstruct(out IEnumerable<T> matches, out IEnumerable<T> nonMatches)
+		{
+			matches = Matches;
+			nonMatches = NonMatches;
+		}
+	}
+}
---
+++ Functional.Tests/Enumerables/PartitionTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Functional.Tests.Enumerables
+{
+	public class PartitionTests
+	{
+		[Fact]
+		public void PartitionSplit()
+		{
+			var (matches, nonMatches) = new[] { 1, 2, 3, 4, 5 }.Partition(i => i % 2 == 0);
+
+			matches.Should().BeEquivalentTo(new[] { 2, 4 });
+			nonMatches.Should().BeEquivalentTo(new[] { 1, 3, 5 });
+		}
+
+		[Fact]
+		public void PartitionAllMatches()
+		{
+			var (matches, nonMatches) = new[] { 1, 2, 3, 4, 5 }.Partition(_ => true);
+
+			matches.Should().BeEquivalentTo(new[] { 1, 2, 3, 4, 5 });
+			nonMatches.Should().BeEquivalentTo(Array.Empty<int>());
+		}
+
+		[Fact]
+		public void PartitionAllNonMatches()
+		{
+			var (matches, nonMatches) = new[] { 1, 2, 3, 4, 5 }.Partition(_ => false);
+
+			matches.Should().BeEquivalentTo(Array.Empty<int>());
+			nonMatches.Should().BeEquivalentTo(new[] { 1, 2, 3, 4, 5 });
+		}
+	}
+}
