528785de22006b3854057fe475cf73fd0d09b2aa
Johannes Moersch <Johannes.Moersch@gmail.com>
2019-05-15 21:27:17 -0600

    Fixes exception when value in dictionary is null and TryGetValue is called.

---
+++ Functional.Tests/Common/DictionaryExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Functional.Tests.Common
+{
+	public class DictionaryExtensionsTests
+	{
+		[Fact]
+		public void TryGetValueWhenKeyPresentAndStringValueNotNull()
+			=> new Dictionary<int, string>() { { 1, "1" } }
+				.TryGetValue(1)
+				.AssertSome()
+				.Should()
+				.Be("1");
+
+		[Fact]
+		public void TryGetValueWhenKeyPresentAndStringValueNull()
+			=> new Dictionary<int, string>() { { 1, null } }
+				.TryGetValue(1)
+				.AssertNone();
+
+		[Fact]
+		public void TryGetValueWhenKeyNotPresent()
+			=> new Dictionary<int, string>()
+				.TryGetValue(1)
+				.AssertNone();
+
+		[Fact]
+		public void TryGetValueWhenKeyPresentAndIntegerValueNotNull()
+			=> new Dictionary<int, int?>() { { 1, 1 } }
+				.TryGetValue(1)
+				.AssertSome()
+				.Should()
+				.Be(1);
+
+		[Fact]
+		public void TryGetValueWhenKeyPresentAndIntegerValueNull()
+			=> new Dictionary<int, int?>() { { 1, null } }
+				.TryGetValue(1)
+				.AssertNone();
+	}
+}
