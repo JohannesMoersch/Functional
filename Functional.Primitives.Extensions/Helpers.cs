92accf7b4b7cd796a3a0936eea7dd77082dd5a93
Johannes Moersch <Johannes.Moersch@gmail.com>
2019-08-15 21:26:07 -0600

    Adds helper methods to help reduce allocations.

---
+++ Functional.Primitives.Extensions/Helpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functional
+{
+	internal static class Helpers
+	{
+		public static bool TryGetValue<TValue>(this Option<TValue> option, out TValue some)
+		{
+			some = option.Match(_ => _, () => default);
+
+			return option.Match(_ => true, () => false);
+		}
+
+		public static bool TryGetValue<TSuccess, TFailure>(this Result<TSuccess, TFailure> result, out TSuccess success, out TFailure failure)
+		{
+			success = result.Match(_ => _, _ => default);
+
+			failure = result.Match(_ => default, _ => _);
+
+			return result.Match(_ => true, _ => false);
+		}
+	}
+}
