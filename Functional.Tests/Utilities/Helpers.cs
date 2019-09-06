62fc6e9f8e808c15941e1d7d3f5e4038c1b96212
Johannes Moersch <Johannes.Moersch@gmail.com>
2019-09-05 21:23:41 -0600

    Adds option factory tests.

---
+++ Functional.Tests/Utilities/Helpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional
+{
+	public static class Helpers
+	{
+		public static async Task<TValue> DelayedTask<TValue>(TValue value)
+		{
+			await Task.Delay(10);
+
+			return value;
+		}
+	}
+}
