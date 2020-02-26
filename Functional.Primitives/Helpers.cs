f3341c3ff2c6f40903baa2723ebe879f5a72bcf7
Ryan Marcotte <ryan.marcotte@live.ca>
2020-02-26 14:20:38 -0600

    don't be clever :)

--- Functional.Primitives/Helpers.cs
+++ Functional.Primitives/Helpers.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Functional
 {
@@ -12,21 +10,12 @@ namespace Functional
 		public static readonly Func<T, T> Passthrough = x => x;
 	}
 
-	internal static class DelegateCache<TIn, TOut>
-	{
-		private static readonly Exception _expectedSuccessException = new InvalidOperationException("Not a successful result!");
-		private static readonly Exception _expectedFailureException = new InvalidOperationException("Not a faulted result!");
-
-		public static readonly Func<TIn, TOut> ExpectedSuccess = _ => throw _expectedSuccessException;
-		public static readonly Func<TIn, TOut> ExpectedFailure = _ => throw _expectedFailureException;
-	}
-
 	internal static class ResultFactoryHelpers
 	{
 		public static TSuccess SuccessUnsafe<TSuccess, TFailure>(this Result<TSuccess, TFailure> source)
-			=> source.Match(DelegateCache<TSuccess>.Passthrough, DelegateCache<TFailure, TSuccess>.ExpectedSuccess);
+			=> source.Match(DelegateCache<TSuccess>.Passthrough, _ => throw new InvalidOperationException("Not a successful result!"));
 
 		public static TFailure FailureUnsafe<TSuccess, TFailure>(this Result<TSuccess, TFailure> source)
-			=> source.Match(DelegateCache<TSuccess, TFailure>.ExpectedFailure, DelegateCache<TFailure>.Passthrough);
+			=> source.Match(_ => new InvalidOperationException("Not a faulted result!"), DelegateCache<TFailure>.Passthrough);
 	}
 }
