ed5365b89e726d58a58b1093189286a1a87fe2c7
IQMETRIXHO\Gabriel.Martinez <gabrielm@iqmetrix.com>
2020-11-18 15:43:09 -0600

    Added Tests for Traverse.

---
+++ Functional.Tests/Enumerables/TraverseTests.cs
@@ -0,0 +1,118 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Functional.Tests.Enumerables
+{
+	public class TraverseTests
+	{
+		[Fact]
+		public void EnumerableTraverseSomes()
+			=> new[]
+			{
+				Option.Some(1),
+				Option.Some(2),
+				Option.Some(3)
+			}
+			.Traverse()
+			.AssertSome()
+			.Should()
+			.BeEquivalentTo(new[] { 1, 2, 3 });
+
+		[Fact]
+		public void EnumerableTraverseNones()
+			=> new[]
+			{
+				Option.None<int>(),
+				Option.Some(2),
+				Option.None<int>()
+			}
+			.Traverse()
+			.AssertNone();
+
+		[Fact]
+		public Task TaskOfEnumerableTraverseSomes()
+			=> Task
+			.FromResult
+			(
+				new[]
+				{
+					Option.Some(1),
+					Option.Some(2),
+					Option.Some(3)
+				}
+				.AsEnumerable()
+			)
+			.Traverse()
+			.AssertSome()
+			.Should()
+			.BeEquivalentTo(new[] { 1, 2, 3 });
+
+		[Fact]
+		public Task TaskOfEnumerableTraverseNones()
+			=> Task
+			.FromResult
+			(
+				new[]
+				{
+					Option.None<int>(),
+					Option.Some(2),
+					Option.None<int>()
+				}
+				.AsEnumerable()
+			)
+			.Traverse()
+			.AssertNone();
+
+		[Fact]
+		public Task EnumerableOfTasksTraverseSomes()
+			=> new[]
+			{
+				Task.FromResult(Option.Some(1)),
+				Task.FromResult(Option.Some(2)),
+				Task.FromResult(Option.Some(3))
+			}
+			.Traverse()
+			.AssertSome()
+			.Should()
+			.BeEquivalentTo(new[] { 1, 2, 3 });
+
+		[Fact]
+		public Task EnumerableOfTasksTraverseNones()
+			=> new[]
+			{
+				Task.FromResult(Option.None<int>()),
+				Task.FromResult(Option.Some(2)),
+				Task.FromResult(Option.None<int>())
+			}
+			.Traverse()
+			.AssertNone();
+
+		[Fact]
+		public Task AsyncEnumerableTraverseSomes()
+			=> new[]
+			{
+				Task.FromResult(Option.Some(1)),
+				Task.FromResult(Option.Some(2)),
+				Task.FromResult(Option.Some(3))
+			}
+			.AsAsyncEnumerable()
+			.Traverse()
+			.AssertSome()
+			.Should()
+			.BeEquivalentTo(new[] { 1, 2, 3 });
+
+		[Fact]
+		public Task AsyncEnumerableTraverseNones()
+			=> new[]
+			{
+				Task.FromResult(Option.None<int>()),
+				Task.FromResult(Option.Some(2)),
+				Task.FromResult(Option.None<int>())
+			}
+			.AsAsyncEnumerable()
+			.Traverse()
+			.AssertNone();
+	}
+}
