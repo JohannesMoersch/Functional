05d02673a971f5f3d96ac34e7434126a79e4f7d9
Ryan Marcotte <ryan.marcotte@live.ca>
2021-01-17 17:28:24 -0600

    add WhereNonEmpty and WhereNonWhiteSpace extension methods for Option<string>

---
+++ Functional.Primitives.Extensions/OptionOfStringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace Functional
+{
+	public static class OptionOfStringExtensions
+	{
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static Option<string> WhereNonEmpty(this Option<string> option)
+			=> option.Where(s => !string.IsNullOrEmpty(s));
+
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static Option<string> WhereNonWhiteSpace(this Option<string> option)
+			=> option.Where(s => !string.IsNullOrWhiteSpace(s));
+
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static async Task<Option<string>> WhereNonEmpty(this Task<Option<string>> option)
+			=> (await option).WhereNonEmpty();
+
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static async Task<Option<string>> WhereNonWhiteSpace(this Task<Option<string>> option)
+			=> (await option).WhereNonWhiteSpace();
+	}
+}
