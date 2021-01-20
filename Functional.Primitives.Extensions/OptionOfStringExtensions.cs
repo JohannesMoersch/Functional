09439ddb3370244a0ec5e8240d4180039a840203
Ryan Marcotte <ryan.marcotte@live.ca>
2021-01-19 21:22:27 -0600

    no allocations

--- Functional.Primitives.Extensions/OptionOfStringExtensions.cs
+++ Functional.Primitives.Extensions/OptionOfStringExtensions.cs
@@ -1,17 +1,22 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Threading.Tasks;
 
 namespace Functional
 {
 	public static class OptionOfStringExtensions
 	{
+		private static readonly Func<string, bool> _whereNonEmptyFunc = s => !string.IsNullOrEmpty(s);
+		private static readonly Func<string, bool> _whereNonWhiteSpaceFunc = s => !string.IsNullOrWhiteSpace(s);
+
+
 		[EditorBrowsable(EditorBrowsableState.Never)]
 		public static Option<string> WhereNonEmpty(this Option<string> option)
-			=> option.Where(s => !string.IsNullOrEmpty(s));
+			=> option.Where(_whereNonEmptyFunc);
 
 		[EditorBrowsable(EditorBrowsableState.Never)]
 		public static Option<string> WhereNonWhiteSpace(this Option<string> option)
-			=> option.Where(s => !string.IsNullOrWhiteSpace(s));
+			=> option.Where(_whereNonWhiteSpaceFunc);
 
 		[EditorBrowsable(EditorBrowsableState.Never)]
 		public static async Task<Option<string>> WhereNonEmpty(this Task<Option<string>> option)
