dbf9ed365f9a84d055c88736050b711dd007e7ab
Johannes Moersch <johannes.moersch@gmail.com>
2023-05-04 23:55:34 -0600

    Removes zip iterators.

--- Functional.Common.Extensions/EnumerableTypeExtensions/EnumerableTypeExtensions.ZipAsync.cs
+++ Functional.Common.Extensions/EnumerableTypeExtensions/EnumerableTypeExtensions.ZipAsync.cs
@@ -6,18 +6,48 @@ namespace Functional;
 
 public static partial class EnumerableTypeExtensions
 {
-	public static IAsyncEnumerable<TResult> ZipAsync<TFirst, TSecond, TResult>(this IEnumerable<TFirst> first, IAsyncEnumerable<TSecond> second, Func<TFirst, TSecond, Task<TResult>> resultSelector)
-		=> AsyncIteratorEnumerable.Create((first, second, resultSelector), static (o, t) => new ZipTaskAsyncIterator<TFirst, TSecond, TResult>(o.first.AsAsyncEnumerable(), o.second, o.resultSelector, t));
+	public static async IAsyncEnumerable<TResult> ZipAsync<TFirst, TSecond, TResult>(this IEnumerable<TFirst> first, IAsyncEnumerable<TSecond> second, Func<TFirst, TSecond, Task<TResult>> resultSelector)
+	{
+		using var e1 = first.GetEnumerator();
+		await using var e2 = second.GetAsyncEnumerator();
 
-	public static IAsyncEnumerable<TResult> ZipAsync<TFirst, TSecond, TResult>(this Task<IEnumerable<TFirst>> first, IAsyncEnumerable<TSecond> second, Func<TFirst, TSecond, Task<TResult>> resultSelector)
-		=> AsyncIteratorEnumerable.Create((first, second, resultSelector), static (o, t) => new ZipTaskAsyncIterator<TFirst, TSecond, TResult>(o.first.AsAsyncEnumerable(), o.second, o.resultSelector, t));
+		while (e1.MoveNext() && await e2.MoveNextAsync())
+			yield return await resultSelector.Invoke(e1.Current, e2.Current);
+	}
 
-	public static IAsyncEnumerable<TResult> ZipAsync<TFirst, TSecond, TResult>(this IAsyncEnumerable<TFirst> first, IEnumerable<TSecond> second, Func<TFirst, TSecond, Task<TResult>> resultSelector)
-		=> AsyncIteratorEnumerable.Create((first, second, resultSelector), static (o, t) => new ZipTaskAsyncIterator<TFirst, TSecond, TResult>(o.first, o.second.AsAsyncEnumerable(), o.resultSelector, t));
+	public static async IAsyncEnumerable<TResult> ZipAsync<TFirst, TSecond, TResult>(this Task<IEnumerable<TFirst>> first, IAsyncEnumerable<TSecond> second, Func<TFirst, TSecond, Task<TResult>> resultSelector)
+	{
+		using var e1 = (await first).GetEnumerator();
+		await using var e2 = second.GetAsyncEnumerator();
 
-	public static IAsyncEnumerable<TResult> ZipAsync<TFirst, TSecond, TResult>(this IAsyncEnumerable<TFirst> first, Task<IEnumerable<TSecond>> second, Func<TFirst, TSecond, Task<TResult>> resultSelector)
-		=> AsyncIteratorEnumerable.Create((first, second, resultSelector), static (o, t) => new ZipTaskAsyncIterator<TFirst, TSecond, TResult>(o.first, o.second.AsAsyncEnumerable(), o.resultSelector, t));
+		while (e1.MoveNext() && await e2.MoveNextAsync())
+			yield return await resultSelector.Invoke(e1.Current, e2.Current);
+	}
 
-	public static IAsyncEnumerable<TResult> ZipAsync<TFirst, TSecond, TResult>(this IAsyncEnumerable<TFirst> first, IAsyncEnumerable<TSecond> second, Func<TFirst, TSecond, Task<TResult>> resultSelector)
-		=> AsyncIteratorEnumerable.Create((first, second, resultSelector), static (o, t) => new ZipTaskAsyncIterator<TFirst, TSecond, TResult>(o.first, o.second, o.resultSelector, t));
+	public static async IAsyncEnumerable<TResult> ZipAsync<TFirst, TSecond, TResult>(this IAsyncEnumerable<TFirst> first, IEnumerable<TSecond> second, Func<TFirst, TSecond, Task<TResult>> resultSelector)
+	{
+		await using var e1 = first.GetAsyncEnumerator();
+		using var e2 = second.GetEnumerator();
+
+		while (await e1.MoveNextAsync() && e2.MoveNext())
+			yield return await resultSelector.Invoke(e1.Current, e2.Current);
+	}
+
+	public static async IAsyncEnumerable<TResult> ZipAsync<TFirst, TSecond, TResult>(this IAsyncEnumerable<TFirst> first, Task<IEnumerable<TSecond>> second, Func<TFirst, TSecond, Task<TResult>> resultSelector)
+	{
+		await using var e1 = first.GetAsyncEnumerator();
+		using var e2 = (await second).GetEnumerator();
+
+		while (await e1.MoveNextAsync() && e2.MoveNext())
+			yield return await resultSelector.Invoke(e1.Current, e2.Current);
+	}
+
+	public static async IAsyncEnumerable<TResult> ZipAsync<TFirst, TSecond, TResult>(this IAsyncEnumerable<TFirst> first, IAsyncEnumerable<TSecond> second, Func<TFirst, TSecond, Task<TResult>> resultSelector)
+	{
+		await using var e1 = first.GetAsyncEnumerator();
+		await using var e2 = second.GetAsyncEnumerator();
+
+		while (await e1.MoveNextAsync() && await e2.MoveNextAsync())
+			yield return await resultSelector.Invoke(e1.Current, e2.Current);
+	}
 }
