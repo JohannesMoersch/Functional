05449745753c68038cb7cb5a86c09e878726762c
Johannes Moersch <johannes.moersch@gmail.com>
2023-05-08 19:59:33 -0600

    Replaces join and group join implementations.

--- Functional.Common.Extensions/EnumerableExtensions/EnumerableExtensions.Join.cs
+++ Functional.Common.Extensions/EnumerableExtensions/EnumerableExtensions.Join.cs
@@ -1,56 +1,133 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System;
-
 namespace Functional;
 
 public static partial class EnumerableExtensions
 {
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IEnumerable<TOuter>> outer, IEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
-		=> (await outer).Join(inner, outerKeySelector, innerKeySelector, resultSelector);
+	public static Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this IEnumerable<TOuter> outer, Task<IEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector, null);
 
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this IEnumerable<TOuter> outer, Task<IEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
-		=> outer.Join(await inner, outerKeySelector, innerKeySelector, resultSelector);
-
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IEnumerable<TOuter>> outer, Task<IEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
-		=> (await outer).Join(await inner, outerKeySelector, innerKeySelector, resultSelector);
-
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IEnumerable<TOuter>> outer, IEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey> comparer)
-		=> (await outer).Join(inner, outerKeySelector, innerKeySelector, resultSelector, comparer);
-
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this IEnumerable<TOuter> outer, Task<IEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey> comparer)
+	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this IEnumerable<TOuter> outer, Task<IEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
 		=> outer.Join(await inner, outerKeySelector, innerKeySelector, resultSelector, comparer);
 
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IEnumerable<TOuter>> outer, Task<IEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey> comparer)
-		=> (await outer).Join(await inner, outerKeySelector, innerKeySelector, resultSelector, comparer);
+	public static Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this IEnumerable<TOuter> outer, Task<IOrderedEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector, null);
 
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IOrderedEnumerable<TOuter>> outer, IEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
-		=> (await outer).Join(inner, outerKeySelector, innerKeySelector, resultSelector);
-
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this IEnumerable<TOuter> outer, Task<IOrderedEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
-		=> outer.Join(await inner, outerKeySelector, innerKeySelector, resultSelector);
-
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IOrderedEnumerable<TOuter>> outer, Task<IEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
-		=> (await outer).Join(await inner, outerKeySelector, innerKeySelector, resultSelector);
-
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IEnumerable<TOuter>> outer, Task<IOrderedEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
-		=> (await outer).Join(await inner, outerKeySelector, innerKeySelector, resultSelector);
-
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IOrderedEnumerable<TOuter>> outer, Task<IOrderedEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
-		=> (await outer).Join(await inner, outerKeySelector, innerKeySelector, resultSelector);
-
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IOrderedEnumerable<TOuter>> outer, IEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey> comparer)
-		=> (await outer).Join(inner, outerKeySelector, innerKeySelector, resultSelector, comparer);
-
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this IEnumerable<TOuter> outer, Task<IOrderedEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey> comparer)
+	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this IEnumerable<TOuter> outer, Task<IOrderedEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
 		=> outer.Join(await inner, outerKeySelector, innerKeySelector, resultSelector, comparer);
 
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IOrderedEnumerable<TOuter>> outer, Task<IEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey> comparer)
+	public static Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this IEnumerable<TOuter> outer, IAsyncEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector, null);
+
+	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this IEnumerable<TOuter> outer, IAsyncEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
+		=> outer.Join(await inner.AsEnumerable(), outerKeySelector, innerKeySelector, resultSelector, comparer);
+
+	public static Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IEnumerable<TOuter>> outer, IEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector, null);
+
+	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IEnumerable<TOuter>> outer, IEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
+		=> (await outer).Join(inner, outerKeySelector, innerKeySelector, resultSelector, comparer);
+
+	public static Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IEnumerable<TOuter>> outer, Task<IEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector, null);
+
+	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IEnumerable<TOuter>> outer, Task<IEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
 		=> (await outer).Join(await inner, outerKeySelector, innerKeySelector, resultSelector, comparer);
 
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IEnumerable<TOuter>> outer, Task<IOrderedEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey> comparer)
+	public static Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IEnumerable<TOuter>> outer, Task<IOrderedEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector, null);
+
+	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IEnumerable<TOuter>> outer, Task<IOrderedEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
 		=> (await outer).Join(await inner, outerKeySelector, innerKeySelector, resultSelector, comparer);
 
-	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IOrderedEnumerable<TOuter>> outer, Task<IOrderedEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey> comparer)
+	public static Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IEnumerable<TOuter>> outer, IAsyncEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector, null);
+
+	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IEnumerable<TOuter>> outer, IAsyncEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
+		=> (await outer).Join(await inner.AsEnumerable(), outerKeySelector, innerKeySelector, resultSelector, comparer);
+
+	public static Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IOrderedEnumerable<TOuter>> outer, IEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector, null);
+
+	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IOrderedEnumerable<TOuter>> outer, IEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
+		=> (await outer).Join(inner, outerKeySelector, innerKeySelector, resultSelector, comparer);
+
+	public static Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IOrderedEnumerable<TOuter>> outer, Task<IEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector, null);
+
+	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IOrderedEnumerable<TOuter>> outer, Task<IEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
 		=> (await outer).Join(await inner, outerKeySelector, innerKeySelector, resultSelector, comparer);
+
+	public static Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IOrderedEnumerable<TOuter>> outer, Task<IOrderedEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector, null);
+
+	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IOrderedEnumerable<TOuter>> outer, Task<IOrderedEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
+		=> (await outer).Join(await inner, outerKeySelector, innerKeySelector, resultSelector, comparer);
+
+	public static Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IOrderedEnumerable<TOuter>> outer, IAsyncEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector, null);
+
+	public static async Task<IEnumerable<TResult>> Join<TOuter, TInner, TKey, TResult>(this Task<IOrderedEnumerable<TOuter>> outer, IAsyncEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
+		=> (await outer).Join(await inner.AsEnumerable(), outerKeySelector, innerKeySelector, resultSelector, comparer);
+
+	public static IAsyncEnumerable<TResult> Join<TOuter, TInner, TKey, TResult>(this IAsyncEnumerable<TOuter> outer, IEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector, null);
+
+	public static async IAsyncEnumerable<TResult> Join<TOuter, TInner, TKey, TResult>(this IAsyncEnumerable<TOuter> outer, IEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
+	{
+		if (inner is null) throw new ArgumentNullException(nameof(inner));
+		if (outerKeySelector is null) throw new ArgumentNullException(nameof(outerKeySelector));
+		if (innerKeySelector is null) throw new ArgumentNullException(nameof(innerKeySelector));
+		if (resultSelector is null) throw new ArgumentNullException(nameof(resultSelector));
+
+		ILookup<TKey, TInner>? lookup = null;
+
+		await foreach (var item in outer)
+		{
+			foreach (var match in (lookup ??= inner.ToLookup(innerKeySelector, comparer))[outerKeySelector.Invoke(item)])
+				yield return resultSelector.Invoke(item, match);
+		}
+	}
+
+	public static IAsyncEnumerable<TResult> Join<TOuter, TInner, TKey, TResult>(this IAsyncEnumerable<TOuter> outer, Task<IEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector, null);
+
+	public static async IAsyncEnumerable<TResult> Join<TOuter, TInner, TKey, TResult>(this IAsyncEnumerable<TOuter> outer, Task<IEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
+	{
+		if (inner is null) throw new ArgumentNullException(nameof(inner));
+		if (outerKeySelector is null) throw new ArgumentNullException(nameof(outerKeySelector));
+		if (innerKeySelector is null) throw new ArgumentNullException(nameof(innerKeySelector));
+		if (resultSelector is null) throw new ArgumentNullException(nameof(resultSelector));
+
+		ILookup<TKey, TInner>? lookup = null;
+
+		await foreach (var item in outer)
+		{
+			foreach (var match in (lookup ??= await inner.ToLookup(innerKeySelector, comparer))[outerKeySelector.Invoke(item)])
+				yield return resultSelector.Invoke(item, match);
+		}
+	}
+
+	public static IAsyncEnumerable<TResult> Join<TOuter, TInner, TKey, TResult>(this IAsyncEnumerable<TOuter> outer, Task<IOrderedEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner.AsEnumerable(), outerKeySelector, innerKeySelector, resultSelector, null);
+
+	public static IAsyncEnumerable<TResult> Join<TOuter, TInner, TKey, TResult>(this IAsyncEnumerable<TOuter> outer, Task<IOrderedEnumerable<TInner>> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
+		=> outer.Join(inner.AsEnumerable(), outerKeySelector, innerKeySelector, resultSelector, comparer);
+
+	public static IAsyncEnumerable<TResult> Join<TOuter, TInner, TKey, TResult>(this IAsyncEnumerable<TOuter> outer, IAsyncEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector)
+		=> outer.Join(inner, outerKeySelector, innerKeySelector, resultSelector, null);
+
+	public static async IAsyncEnumerable<TResult> Join<TOuter, TInner, TKey, TResult>(this IAsyncEnumerable<TOuter> outer, IAsyncEnumerable<TInner> inner, Func<TOuter, TKey> outerKeySelector, Func<TInner, TKey> innerKeySelector, Func<TOuter, TInner, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
+	{
+		if (inner is null) throw new ArgumentNullException(nameof(inner));
+		if (outerKeySelector is null) throw new ArgumentNullException(nameof(outerKeySelector));
+		if (innerKeySelector is null) throw new ArgumentNullException(nameof(innerKeySelector));
+		if (resultSelector is null) throw new ArgumentNullException(nameof(resultSelector));
+
+		ILookup<TKey, TInner>? lookup = null;
+
+		await foreach (var item in outer)
+		{
+			foreach (var match in (lookup ??= await inner.ToLookup(innerKeySelector, comparer))[outerKeySelector.Invoke(item)])
+				yield return resultSelector.Invoke(item, match);
+		}
+	}
 }
--- Functional.Common.Extensions/EnumerableExtensions/EnumerableExtensions.ToLookup.cs
+++ Functional.Common.Extensions/EnumerableExtensions/EnumerableExtensions.ToLookup.cs
@@ -10,24 +10,36 @@ public static partial class EnumerableExtensions
 	public static async Task<ILookup<TKey, TSource>> ToLookup<TSource, TKey>(this Task<IEnumerable<TSource>> source, Func<TSource, TKey> keySelector)
 		=> (await source).ToLookup(keySelector);
 
-	public static async Task<ILookup<TKey, TSource>> ToLookup<TSource, TKey>(this Task<IEnumerable<TSource>> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
+	public static async Task<ILookup<TKey, TSource>> ToLookup<TSource, TKey>(this Task<IEnumerable<TSource>> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey>? comparer)
 		=> (await source).ToLookup(keySelector, comparer);
 
 	public static async Task<ILookup<TKey, TElement>> ToLookup<TSource, TKey, TElement>(this Task<IEnumerable<TSource>> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector)
 		=> (await source).ToLookup(keySelector, elementSelector);
 
-	public static async Task<ILookup<TKey, TElement>> ToLookup<TSource, TKey, TElement>(this Task<IEnumerable<TSource>> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, IEqualityComparer<TKey> comparer)
+	public static async Task<ILookup<TKey, TElement>> ToLookup<TSource, TKey, TElement>(this Task<IEnumerable<TSource>> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, IEqualityComparer<TKey>? comparer)
 		=> (await source).ToLookup(keySelector, elementSelector, comparer);
 
 	public static async Task<ILookup<TKey, TSource>> ToLookup<TSource, TKey>(this Task<IOrderedEnumerable<TSource>> source, Func<TSource, TKey> keySelector)
 		=> (await source).ToLookup(keySelector);
 
-	public static async Task<ILookup<TKey, TSource>> ToLookup<TSource, TKey>(this Task<IOrderedEnumerable<TSource>> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
+	public static async Task<ILookup<TKey, TSource>> ToLookup<TSource, TKey>(this Task<IOrderedEnumerable<TSource>> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey>? comparer)
 		=> (await source).ToLookup(keySelector, comparer);
 
 	public static async Task<ILookup<TKey, TElement>> ToLookup<TSource, TKey, TElement>(this Task<IOrderedEnumerable<TSource>> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector)
 		=> (await source).ToLookup(keySelector, elementSelector);
 
-	public static async Task<ILookup<TKey, TElement>> ToLookup<TSource, TKey, TElement>(this Task<IOrderedEnumerable<TSource>> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, IEqualityComparer<TKey> comparer)
+	public static async Task<ILookup<TKey, TElement>> ToLookup<TSource, TKey, TElement>(this Task<IOrderedEnumerable<TSource>> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, IEqualityComparer<TKey>? comparer)
 		=> (await source).ToLookup(keySelector, elementSelector, comparer);
+
+	public static Task<ILookup<TKey, TSource>> ToLookup<TSource, TKey>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+		=> source.AsEnumerable().ToLookup(keySelector);
+
+	public static Task<ILookup<TKey, TSource>> ToLookup<TSource, TKey>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey>? comparer)
+		=> source.AsEnumerable().ToLookup(keySelector, comparer);
+
+	public static Task<ILookup<TKey, TElement>> ToLookup<TSource, TKey, TElement>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector)
+		=> source.AsEnumerable().ToLookup(keySelector, elementSelector);
+
+	public static Task<ILookup<TKey, TElement>> ToLookup<TSource, TKey, TElement>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, IEqualityComparer<TKey>? comparer)
+		=> source.AsEnumerable().ToLookup(keySelector, elementSelector, comparer);
 }
