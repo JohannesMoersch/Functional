5520b0a72383b02349aa013dcbec4bd46f963e46
Johannes Moersch <johannes.moersch@gmail.com>
2023-05-12 15:13:25 -0600

    Adds missing skipwhileasync and takewhileasync methods.

--- Functional.Common.Extensions/EnumerableExtensions/EnumerableExtensions.SkipWhileAsync.cs
+++ Functional.Common.Extensions/EnumerableExtensions/EnumerableExtensions.SkipWhileAsync.cs
@@ -6,17 +6,61 @@ namespace Functional;
 
 public static partial class EnumerableExtensions
 {
-	public static IAsyncEnumerable<TSource> SkipWhileAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, Task<bool>> predicate)
-		=> source.AsAsyncEnumerable().SkipWhileAsync(predicate);
+	public static async IAsyncEnumerable<TSource> SkipWhileAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, Task<bool>> predicate)
+	{
+		if (predicate == null) throw new ArgumentNullException(nameof(predicate));
 
-	public static IAsyncEnumerable<TSource> SkipWhileAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, int, Task<bool>> predicate)
-		=> source.AsAsyncEnumerable().SkipWhileAsync(predicate);
+		bool skipping = true;
+		foreach (var item in source)
+		{
+			if (!skipping || !(skipping = await predicate.Invoke(item)))
+				yield return item;
+		}
+	}
 
-	public static IAsyncEnumerable<TSource> SkipWhileAsync<TSource>(this Task<IEnumerable<TSource>> source, Func<TSource, Task<bool>> predicate)
-		=> source.AsAsyncEnumerable().SkipWhileAsync(predicate);
+	public static async IAsyncEnumerable<TSource> SkipWhileAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, int, Task<bool>> predicate)
+	{
+		if (predicate == null) throw new ArgumentNullException(nameof(predicate));
 
-	public static IAsyncEnumerable<TSource> SkipWhileAsync<TSource>(this Task<IEnumerable<TSource>> source, Func<TSource, int, Task<bool>> predicate)
-		=> source.AsAsyncEnumerable().SkipWhileAsync(predicate);
+		bool skipping = true;
+		int index = 0;
+		foreach (var item in source)
+		{
+			if (!skipping || !(skipping = await predicate.Invoke(item, index++)))
+				yield return item;
+		}
+	}
+
+	public static async IAsyncEnumerable<TSource> SkipWhileAsync<TSource>(this Task<IEnumerable<TSource>> source, Func<TSource, Task<bool>> predicate)
+	{
+		if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+		bool skipping = true;
+		foreach (var item in await source)
+		{
+			if (!skipping || !(skipping = await predicate.Invoke(item)))
+				yield return item;
+		}
+	}
+
+	public static async IAsyncEnumerable<TSource> SkipWhileAsync<TSource>(this Task<IEnumerable<TSource>> source, Func<TSource, int, Task<bool>> predicate)
+	{
+		if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+		bool skipping = true;
+		int index = 0;
+		foreach (var item in await source)
+		{
+			if (!skipping || !(skipping = await predicate.Invoke(item, index++)))
+				yield return item;
+		}
+	}
+
+	public static IAsyncEnumerable<TSource> SkipWhileAsync<TSource>(this Task<IOrderedEnumerable<TSource>> source, Func<TSource, Task<bool>> predicate)
+		=> source.AsEnumerable().SkipWhileAsync(predicate);
+
+	public static IAsyncEnumerable<TSource> SkipWhileAsync<TSource>(this Task<IOrderedEnumerable<TSource>> source, Func<TSource, int, Task<bool>> predicate)
+		=> source.AsEnumerable().SkipWhileAsync(predicate);
 
 	public static async IAsyncEnumerable<TSource> SkipWhileAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, Task<bool>> predicate)
 	{
--- Functional.Common.Extensions/EnumerableExtensions/EnumerableExtensions.TakeWhileAsync.cs
+++ Functional.Common.Extensions/EnumerableExtensions/EnumerableExtensions.TakeWhileAsync.cs
@@ -6,17 +6,65 @@ namespace Functional;
 
 public static partial class EnumerableExtensions
 {
-	public static IAsyncEnumerable<TSource> TakeWhileAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, Task<bool>> predicate)
-		=> source.AsAsyncEnumerable().TakeWhileAsync(predicate);
+	public static async IAsyncEnumerable<TSource> TakeWhileAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, Task<bool>> predicate)
+	{
+		if (predicate == null) throw new ArgumentNullException(nameof(predicate));
 
-	public static IAsyncEnumerable<TSource> TakeWhileAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, int, Task<bool>> predicate)
-		=> source.AsAsyncEnumerable().TakeWhileAsync(predicate);
+		foreach (var item in source)
+		{
+			if (!await predicate.Invoke(item))
+				yield break;
 
-	public static IAsyncEnumerable<TSource> TakeWhileAsync<TSource>(this Task<IEnumerable<TSource>> source, Func<TSource, Task<bool>> predicate)
-		=> source.AsAsyncEnumerable().TakeWhileAsync(predicate);
+			yield return item;
+		}
+	}
 
-	public static IAsyncEnumerable<TSource> TakeWhileAsync<TSource>(this Task<IEnumerable<TSource>> source, Func<TSource, int, Task<bool>> predicate)
-		=> source.AsAsyncEnumerable().TakeWhileAsync(predicate);
+	public static async IAsyncEnumerable<TSource> TakeWhileAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, int, Task<bool>> predicate)
+	{
+		if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+		int index = 0;
+		foreach (var item in source)
+		{
+			if (!await predicate.Invoke(item, index++))
+				yield break;
+
+			yield return item;
+		}
+	}
+
+	public static async IAsyncEnumerable<TSource> TakeWhileAsync<TSource>(this Task<IEnumerable<TSource>> source, Func<TSource, Task<bool>> predicate)
+	{
+		if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+		foreach (var item in await source)
+		{
+			if (!await predicate.Invoke(item))
+				yield break;
+
+			yield return item;
+		}
+	}
+
+	public static async IAsyncEnumerable<TSource> TakeWhileAsync<TSource>(this Task<IEnumerable<TSource>> source, Func<TSource, int, Task<bool>> predicate)
+	{
+		if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+		int index = 0;
+		foreach (var item in await source)
+		{
+			if (!await predicate.Invoke(item, index++))
+				yield break;
+
+			yield return item;
+		}
+	}
+
+	public static IAsyncEnumerable<TSource> TakeWhileAsync<TSource>(this Task<IOrderedEnumerable<TSource>> source, Func<TSource, Task<bool>> predicate)
+		=> source.AsEnumerable().TakeWhileAsync(predicate);
+
+	public static IAsyncEnumerable<TSource> TakeWhileAsync<TSource>(this Task<IOrderedEnumerable<TSource>> source, Func<TSource, int, Task<bool>> predicate)
+		=> source.AsEnumerable().TakeWhileAsync(predicate);
 
 	public static async IAsyncEnumerable<TSource> TakeWhileAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, Task<bool>> predicate)
 	{
