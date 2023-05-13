c3ac72eb208686a17130dba311db6de799d9864c
Johannes Moersch <johannes.moersch@gmail.com>
2023-05-13 13:14:58 -0600

    Adds groupby extensions.

--- Functional.Common.Extensions/EnumerableExtensions/EnumerableExtensions.GroupBy.cs
+++ Functional.Common.Extensions/EnumerableExtensions/EnumerableExtensions.GroupBy.cs
@@ -54,4 +54,28 @@ public static partial class EnumerableExtensions
 
 	public static async Task<IEnumerable<IGrouping<TKey, TElement>>> GroupBy<TSource, TKey, TElement>(this Task<IOrderedEnumerable<TSource>> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, IEqualityComparer<TKey>? comparer)
 		=> (await source).GroupBy(keySelector, elementSelector, comparer);
+
+	public static async Task<IEnumerable<TResult>> GroupBy<TSource, TKey, TElement, TResult>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, Func<TKey, IEnumerable<TElement>, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
+		=> (await source.AsEnumerable()).GroupBy(keySelector, elementSelector, resultSelector, comparer);
+
+	public static async Task<IEnumerable<TResult>> GroupBy<TSource, TKey, TElement, TResult>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, Func<TKey, IEnumerable<TElement>, TResult> resultSelector)
+		=> (await source.AsEnumerable()).GroupBy(keySelector, elementSelector, resultSelector);
+
+	public static async Task<IEnumerable<TResult>> GroupBy<TSource, TKey, TResult>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TKey, IEnumerable<TSource>, TResult> resultSelector, IEqualityComparer<TKey>? comparer)
+		=> (await source.AsEnumerable()).GroupBy(keySelector, resultSelector, comparer);
+
+	public static async Task<IEnumerable<TResult>> GroupBy<TSource, TKey, TResult>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TKey, IEnumerable<TSource>, TResult> resultSelector)
+		=> (await source.AsEnumerable()).GroupBy(keySelector, resultSelector);
+
+	public static async Task<IEnumerable<IGrouping<TKey, TSource>>> GroupBy<TSource, TKey>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey>? comparer)
+		=> (await source.AsEnumerable()).GroupBy(keySelector, comparer);
+
+	public static async Task<IEnumerable<IGrouping<TKey, TElement>>> GroupBy<TSource, TKey, TElement>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector)
+		=> (await source.AsEnumerable()).GroupBy(keySelector, elementSelector);
+
+	public static async Task<IEnumerable<IGrouping<TKey, TSource>>> GroupBy<TSource, TKey>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+		=> (await source.AsEnumerable()).GroupBy(keySelector);
+
+	public static async Task<IEnumerable<IGrouping<TKey, TElement>>> GroupBy<TSource, TKey, TElement>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, IEqualityComparer<TKey>? comparer)
+		=> (await source.AsEnumerable()).GroupBy(keySelector, elementSelector, comparer);
 }
